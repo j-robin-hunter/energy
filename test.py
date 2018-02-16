fc2de241fa471eaa357fb5d4ee96ae86c1389531
Robin Hunter <robin@roma.eu.com>
2018-02-16 16:27:26 +0000

    Committed after trial testing

--- test.py
+++ test.py
@@ -1,7 +1,31 @@
+import json
+from pprint import pprint
 import socket
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.bind(("0.0.0.0", 53005))
-while True:
-  data, addr = sock.recvfrom(4096)
-  print(data)
+def readConfig(file):
+  with open(file) as configFile:
+    config = json.loads(configFile.read())
+
+  return config['meters']
+
+def getNum(x):
+    return int(''.join(ele for ele in x if ele.isdigit()))
+
+def readData(meters):
+  sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+  sock.bind(("0.0.0.0", 53005))
+  while True:
+    try:
+      data, addr = sock.recvfrom(4096)
+      index = data.find(b"D1")
+      if index > 0:
+        tokens = data[index:len(data)].decode("utf-8").split(",")
+        if len(tokens) >= 6:
+          power = getNum(tokens[3])
+          meter = list(filter(lambda meter: meter['channel'] == getNum(tokens[5]), meters))
+          print("%s = %d watts, contributing to %s" % (meter[0].get("name"), power / 1000, meter[0].get("category")))
+
+    except Exception as e:
+        print(e)
+
+readData(readConfig("C:\\Users\\Robin Hunter\\PycharmProjects\\energy\\config.json"))
